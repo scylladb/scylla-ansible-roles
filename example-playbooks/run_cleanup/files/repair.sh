65c7dc08e3f5a104dbcf02c32a50a1a28382bce1
Dan Yasny <dyasny@scylladb.com>
2021-01-12 17:42:31 -0500

    bring back repair.sh for the cleanup playbook

---
+++ example-playbooks/run_cleanup/files/repair.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$(grep -A1 data_file_directories /etc/scylla/scylla.yaml|grep -v 'data_file_directories:'|awk '{ print $NF }')
+
+cd $DIR
+pwd
+
+PATHS=$(du -m --max-depth=2 .|sort -k1h|awk -F"/" 'NF==3'|awk '{ print $NF}'|egrep -v '^./system/'\|'^./system_auth/'\|'^./system_distributed/'\|'^./system_traces/'\|'^./system_schema/')
+
+for i in $PATHS; do
+    TB=$(echo $i|awk -F'/' '{ print $NF }'|awk -F'-' '{ print $1 }')
+    KS=$(echo $i|awk -F'/' '{ print $(NF - 1) }')
+    nodetool cleanup $KS $TB
+done
