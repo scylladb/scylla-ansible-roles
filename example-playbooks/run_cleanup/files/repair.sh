caaf53333f20d3e5bb40062f805df68711bbe3fc
Lubos Kosco <lubos@scylladb.com>
2021-03-12 14:17:53 +0100

    more verbosity to cleanup

--- example-playbooks/run_cleanup/files/repair.sh
+++ example-playbooks/run_cleanup/files/repair.sh
@@ -5,10 +5,12 @@ DIR=$(grep -A1 data_file_directories /etc/scylla/scylla.yaml|grep -v 'data_file_
 cd $DIR
 pwd
 
+echo "running on $HOSTNAME"
 PATHS=$(du -m --max-depth=2 .|sort -k1h|awk -F"/" 'NF==3'|awk '{ print $NF}'|egrep -v '^./system/'\|'^./system_auth/'\|'^./system_distributed/'\|'^./system_traces/'\|'^./system_schema/')
 
 for i in $PATHS; do
     TB=$(echo $i|awk -F'/' '{ print $NF }'|awk -F'-' '{ print $1 }')
     KS=$(echo $i|awk -F'/' '{ print $(NF - 1) }')
+    echo "cleanup of $KS $TB"
     nodetool cleanup $KS $TB
 done
