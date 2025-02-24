876b2125e63e25201b719d3103bfad1d32d16029
Vlad Zolotarov <vladz@scylladb.com>
2025-02-24 15:09:27 -0500

    async_extra: install.sh: add cleaning ~/.ansible/plugins/action/__pycache__
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- example-playbooks/async_extra/install.sh
+++ example-playbooks/async_extra/install.sh
@@ -3,6 +3,7 @@
 
 mkdir -p ~/.ansible/plugins/action
 mkdir -p ~/.ansible/plugins/modules
+rm ~/.ansible/plugins/action/__pycache__/*
 
 cp ./library/* ~/.ansible/plugins/modules/
 cp ./action_plugins/* ~/.ansible/plugins/action/
