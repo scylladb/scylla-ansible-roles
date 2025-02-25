3f936715ae752c833c237d036241c052d64c6055
Vlad Zolotarov <vladz@scylladb.com>
2025-02-25 16:52:30 -0500

    async_extra/tests: use 'python3' instead of 'python'
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- example-playbooks/async_extra/test_async_status_id.sh
+++ example-playbooks/async_extra/test_async_status_id.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "{ \"ANSIBLE_MODULE_ARGS\": { \"alias\": \"$1\", \"_async_dir\": \"$HOME/.ansible_async\" } }" | PYTHONPATH=library python -m async_status_id
+echo "{ \"ANSIBLE_MODULE_ARGS\": { \"alias\": \"$1\", \"_async_dir\": \"$HOME/.ansible_async\" } }" | PYTHONPATH=library python3 -m async_status_id
